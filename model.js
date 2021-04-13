b99530b5ee296246fb6f202a816246b7d7bbc4e9
“NamasteGeek ”git config --global user.email “techchic90@gmail.com ”git config --global user.name “NamasteGeek <40542070+namastegeek@users.noreply.github.com>
2021-04-12 22:18:20 -0500

    April12 In Class work

---
+++ model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+var contactSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    gender: String,
+    phone: String,
+    create_date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+var Contact = module.exports = mongoose.model("contact2", contactSchema);
+
+//missing one line
